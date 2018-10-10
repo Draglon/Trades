46670e56cd09f7b6500f37b98ac5127e66b777cb
Sergey <s.nazarchuck@gmail.com>
2018-10-10 11:55:18 +0300

    add home page

--- app/js/App.js
+++ app/js/App.js
@@ -18,8 +18,10 @@ class App extends Component {
                     <div className='wrapper'>
                         <div className='header__left-side'>
                             <div className='logo'>
-                                <img className='logo__img' src={img} alt='Trades' />
-                                <span className='logo__name'>Trades</span>
+                                <a className='logo__link' href='#'>
+                                    <img className='logo__img' src={img} alt='Trades' />
+                                    <span className='logo__name'>Trades</span>
+                                </a>
                             </div>
                             <nav className='navbar'>
                                 <ul className='navbar__menu'>
@@ -33,71 +35,86 @@ class App extends Component {
 
                         <div className='header__right-side'>
                             <nav className='header__nav'>
-                                <div className=''>
-                                    <a href='#'>Русский</a>
-                                </div>
-                                {/* <ul className='lang'>
-                                    <li className='lang__item'>
-                                        
-                                    </li>
-                                    <li className='lang__item'>
-                                        
-                                    </li>
-                                    <li className='lang__item'>
-                                        
-                                    </li>
-                                </ul> */}
-
-                                <div className=''>
-                                    <i className='icon-bell'></i>
+                                <div className='lang'>
+                                    <a className='lang__link' href='#' title='Language'>Русский</a>
+                                    {/* 
+                                        <ul className='lang'>
+                                            <li className='lang__item'>
+                                                
+                                            </li>
+                                            <li className='lang__item'>
+                                                
+                                            </li>
+                                            <li className='lang__item'>
+                                                
+                                            </li>
+                                        </ul>
+                                     */}
                                 </div>
 
-                                <div className=''>
-                                    <a href='#'>test321</a>
+                                <div className='notification'>
+                                    <a className='notification__link' href='#' title='Notification'>
+                                        <i className='icon-bell'></i>
+                                        <span class="notification__count">2</span>
+                                    </a>
+                                </div>
+
+                                <div className='profile'>
+                                    <a className='profile__link' href='#' title='Profile'>test321</a>
                                 </div>
                             </nav>
                         </div>
                     </div>
                 </header>
-                <main>
-                    
+
+                <main className='main'>
+                    <section className='section invitation'>
+                        <div className='wrapper'>
+                            <h1 className='invitation__title'>Invitation</h1>
+                        </div>
+                    </section>
                 </main>
+
                 <footer className='footer'>
-                    <div className='footer__item'>
-                        <h3 className='footer__title'>Продукты</h3>
-                        <ul className='footer__list'>
-                            <li><a href='#'>Кошелек</a></li>
-                            <li><a href='#'>Торги</a></li>
-                            <li><a href='#'>Обмен</a></li>
-                            <li><a href='#'>API</a></li>
-                        </ul>
+                    <div className='wrapper'>
+                        <nav className='footer__nav'>
+                            <div className='footer__item'>
+                                <h3 className='footer__title'>Продукты</h3>
+                                <ul className='footer__list'>
+                                    <li><a href='#'>Кошелек</a></li>
+                                    <li><a href='#'>Торги</a></li>
+                                    <li><a href='#'>Обмен</a></li>
+                                    <li><a href='#'>API</a></li>
+                                </ul>
+                            </div>
+                            <div className='footer__item'>
+                                <h3 className='footer__title'>Информация</h3>
+                                <ul className='footer__list'>
+                                    <li><a href='#'>Комисии и лимиты</a></li>
+                                    <li><a href='#'>Реферальная программа</a></li>
+                                    <li><a href='#'>Служба поддержки</a></li>
+                                </ul>
+                            </div>
+                            <div className='footer__item'>
+                                <h3 className='footer__title'>Компания</h3>
+                                <ul className='footer__list'>
+                                    <li><a href='#'>О нас</a></li>
+                                    <li><a href='#'>Контакты</a></li>
+                                    <li><a href='#'>Новости</a></li>
+                                </ul>
+                            </div>
+                            <div className='footer__item'>
+                                <h3 className='footer__title'>Документы</h3>
+                                <ul className='footer__list'>
+                                    <li><a href='#'>Договор оферты</a></li>
+                                    <li><a href='#'>Политика конфиденциальности</a></li>
+                                    <li><a href='#'>AML</a></li>
+                                    <li><a href='#'>Пользовательское соглашение</a></li>
+                                </ul>
+                            </div>
+                        </nav>
+                        <div className='footer__copy'>2018 &copy; Trades.pp.ua</div>
                     </div>
-                    <div className='footer__item'>
-                        <h3 className='footer__title'>Информация</h3>
-                        <ul className='footer__list'>
-                            <li><a href='#'>Комисии и лимиты</a></li>
-                            <li><a href='#'>Реферальная программа</a></li>
-                            <li><a href='#'>Служба поддержки</a></li>
-                        </ul>
-                    </div>
-                    <div className='footer__item'>
-                        <h3 className='footer__title'>Компания</h3>
-                        <ul className='footer__list'>
-                            <li><a href='#'>О нас</a></li>
-                            <li><a href='#'>Контакты</a></li>
-                            <li><a href='#'>Новости</a></li>
-                        </ul>
-                    </div>
-                    <div className='footer__item'>
-                        <h3 className='footer__title'>Документы</h3>
-                        <ul className='footer__list'>
-                            <li><a href='#'>Договор оферты</a></li>
-                            <li><a href='#'>Политика конфиденциальности</a></li>
-                            <li><a href='#'>AML</a></li>
-                            <li><a href='#'>Пользовательское соглашение</a></li>
-                        </ul>
-                    </div>
-                    <div className='footer__copy'>2018 &copy; Trades.pp.ua</div>
                 </footer>
             </div>
         );

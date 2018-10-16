228b2a4860e5583a3e9072ed510d378c693bc647
Sergey <s.nazarchuck@gmail.com>
2018-10-16 17:52:29 +0300

    fix

--- app/js/pages/Wallet.js
+++ app/js/pages/Wallet.js
@@ -170,6 +170,63 @@ class Wallet extends React.Component{
                         </div>
                     </div>
                 </section>
+
+                <section className='section refill'>
+                    <div className='wrapper'>
+                        <a className='back-link' href='#'>Вернуться к выбору валют</a>
+                        <h2 className='refill__title'>Пополнение счета</h2>
+                        <div className='refill__item'>
+                            <div className='payments'>
+                                <h3 className='payments__title'>Выберите способ пополнения баланса</h3>
+                                <div className='payments__lest'>
+                                    <div className='payments__item'>
+                                        <label className='payments__label' for='mastercard'>
+                                            <span className='payments__text'>Visa/MasterCard (Simplex)</span>
+                                            <input className='payments__input' type='checkbox' id='mastercard' />
+                                        </label>
+                                    </div>
+                                    <div className='payments__item'>
+                                        <label className='payments__label' for='skrill'>
+                                            <span className='payments__text'>Skrill</span>
+                                            <input className='payments__input' type='checkbox' id='skrill' />
+                                        </label>
+                                    </div>
+                                    <div className='payments__item'>
+                                        <label className='payments__label' for='advcash'>
+                                            <span className='payments__text'>AdvCash</span>
+                                            <input className='payments__input' type='checkbox' id='advcash' />
+                                        </label>
+                                    </div>
+                                    <div className='payments__item'>
+                                        <label className='payments__label' for='payeer'>
+                                            <span className='payments__text'>Payeer</span>
+                                            <input className='payments__input' type='checkbox' id='payeer' />
+                                        </label>
+                                    </div>
+                                    <div className='payments__item'>
+                                        <label className='payments__label' for='neteller'>
+                                            <span className='payments__text'>Neteller</span>
+                                            <input className='payments__input' type='checkbox' id='neteller' />
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='refill__item'>
+                            <div className='payments'>
+                                <h3 className='payments__title'>Нужно пройти верификацию</h3>
+                                <div className='payments__deposit'>Минимальная сумма депозита 0.01 DASH</div>
+                                <ul className='payments__commission'>
+                                    <li className='payments__commission-item'>Минимальная 0.01 DASH</li>
+                                    <li className='payments__commission-item'>Комиссия 0%</li>
+                                </ul>
+                                <div className='payments__btns'>
+                                    <button className='btn btn-primary'>Создать адрес</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </main>
         );
     }
